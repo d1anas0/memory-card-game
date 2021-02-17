0e2244b5425adf5ba6a9bc5bd6aaded8624808a1
d1anas0 <231689@gmail.com>
2021-02-17 17:42:56 +1100

    add restart button

--- scripts.js
+++ scripts.js
@@ -60,15 +60,16 @@ cards.forEach(card => {
 // ADDED FEATURES
 
 // 1. Add button to reset game without refreshing browser
-const play = document.querySelector('#play');
+const playBtn = document.querySelector('#playBtn');
 
-play.addEventListener('click', () => {
+playBtn.addEventListener('click', () => {
     cards.forEach(card => {
         card.classList.remove('flip');
         card.addEventListener('click', flipCard);
     })
     shuffle();
     lockBoard = false;
+    playBtn.innerText = 'Restart';
 
 // 2. Add Countdown timer (till game end)
     let barWidth = 640;
@@ -86,6 +87,21 @@ play.addEventListener('click', () => {
                 let newBarWidth = barWidth - reduceBarBy;
                 progressBar.style.width = newBarWidth + 'px';
                 console.log(barWidth);
+
+                
+                // 3. restart game before time ends button
+                if (time_left > 0) {
+                    playBtn.onclick = () => {
+                        clearInterval(interval);
+                        cards.forEach(card => {
+                            card.classList.remove('flip');
+                        })
+                        shuffle();
+                        lockBoard = false;
+                        newBarWidth = barWidth;    
+                        progressBar.style.width = newBarWidth + 'px';   
+                    }
+                }
                 
                 // 2a. Stop game once time is up, reset board.
 
@@ -98,10 +114,29 @@ play.addEventListener('click', () => {
                     lockBoard = true;
                     newBarWidth = barWidth;    
                     progressBar.style.width = newBarWidth + 'px';           
-                    progressBar.innerText = "Oops - Too slow! Click START to try again..."
+                    progressBar.innerText = "Play again?"
                 }
             }, 1000);
         };
     startCountdown(30);
+
+
 });
+
+
+
+// 4. end and restart game when all pairs found
+// if all the cards have been flipped (ie. all pairs matched)
+// success = () => {
+//     cards.forEach(card => {
+//         card.classList.contains('flip');
+        
+//     })
+// }
+// if (success() === true) {
+
+// }
+
+// scorecard pops up with time taken
+// game resets
 
